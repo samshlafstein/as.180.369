6495d41beb2546fe06d6e0b383ed5b6ea69a1711
Sam Shlafstein <sshlafs1@jh.edu>
2024-12-06 22:29:47 -0500

    Delete contrib/samshlafstein/Paper/02_reproduce.sh

--- contrib/samshlafstein/Paper/02_reproduce.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Run the Jupyter notebook and execute all cells in place
-jupyter nbconvert --to notebook --execute --inplace 05_randomforest.ipynb
-
-# Build the PDF using myst
-myst build --pdf
